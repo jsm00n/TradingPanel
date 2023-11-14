5fdd904bf66c11e65b2fc62dfffd83c61d060176
m00n <js.m00n@outlook.com>
2023-11-14 02:20:11 -0600

    added warning button in taskitem

--- src/widgets/Tasks/TaskItem/TaskItemBottom.tsx
+++ src/widgets/Tasks/TaskItem/TaskItemBottom.tsx
@@ -2,7 +2,10 @@ import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { Badge } from "@/components/ui/badge"
 import TaskBottomDetailItem from "./TaskBottomDetail"
-import React, { useState } from 'react'
+import { useState } from 'react'
+import { MdWarning } from "react-icons/md";
+import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 
 const TaskItemBottom = () => {
 
@@ -13,26 +16,33 @@ const TaskItemBottom = () => {
     }
 
     return (
-        <div className="flex-row h-full pl-8 pr-4">
+        <div className="flex-row h-full pl-5 pr-4">
             <div className="flex my-auto h-10 justify-between space-x-3">
-                <div className="flex my-auto space-x-4 text-[13px] font-normal">
-                    <div className="my-auto text-xs m-md:text-sm">Defi Task</div>
+                <div className="flex my-auto space-x-4 font-normal">
+                    <div className="my-auto text-[11px] m-md:text-[13px]">Defi Task</div>
                     <Separator className="h-6" orientation="vertical"></Separator>
                     <div className="flex my-auto">
-                        <div className="text-xs m-md:text-sm">Wallets</div>
+                        <div className="text-[11px] m-md:text-[13px]">Wallets</div>
                         <Badge className="my-auto ml-1 pl-1.5 pr-1.5 pb-0 pt-0 bg-gray-600" variant="secondary">5</Badge>
                     </div>
                     <Separator className="h-6" orientation="vertical"></Separator>
-                    <div className="my-auto text-xs m-md:text-sm">Total Wallet Balance 0.2</div>
+                    <div className="my-auto text-[11px] m-md:text-[13px]">Total Wallet Balance 0.2</div>
                 </div>
-                <Button
-                    name="expand"
-                    className="my-auto font-normal text-[12px]"
-                    variant="link"
-                    onClick={handleToggle}
-                >
-                    {isExpanded ? "Hide Task Details" : "Show Task Details"}
-                </Button>
+                <div className="flex">
+                    <Button className="my-auto pl-1.5 pr-1.5 pt-0 pb-0 h-4.5  rounded-lg bg-yellow-600 text-white font-normal text-[10px] m-md:text-[11px]">
+                        <MdWarning className="my-auto w-3 h-3 mr-1"></MdWarning>
+                        2 Warnings
+                    </Button>
+                    <Button
+                        name="expand"
+                        className="my-auto font-normal text-[11px] m-md:text-[12px]"
+                        variant="link"
+                        onClick={handleToggle}
+                    >
+                        {isExpanded ? "Hide Task Details" : "Show Task Details"}
+                    </Button>
+                    {isExpanded ? <IoIosArrowUp className="my-auto"></IoIosArrowUp> : <IoIosArrowDown className="my-auto"></IoIosArrowDown>}
+                </div>
             </div >
             {isExpanded ? <Separator className="w-full" orientation="horizontal"></Separator> : <></>}
             <TaskBottomDetailItem isActive={isExpanded} />
